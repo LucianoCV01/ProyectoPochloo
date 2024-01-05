ec254da577d0d5eeabb8804853e5148a5e2cf7e4
LucianoCV01 <Lucianocosentino01@gmail.com>
2024-01-05 18:18:27 -0300

    Funcion agregar terminada, falta agregar modal

--- js/index.js
+++ js/index.js
@@ -4,22 +4,16 @@ class Producto {
         this.nombre = nombre;
         this.categoria = categoria;
         this.descripcion = descripcion;
-        this._publicado = publicado;
-        this._destacado = destacado;
+        this.publicado = publicado;
+        this.destacado = destacado;
         this.imagen = imagen;
         this.url = url;
     }
-
-    set publicado(valor) {
-        this._publicado = valor;
-    }
-    set destacado(valor) {
-        this._destacado = valor;
-    }
 }
 
-let pelicula1 = new Producto(1234, "Pelicula", "Categoria", "Descripcion", false, false, "Link", "Link");
+//let pelicula1 = new Producto(1234, "Pelicula", "Categoria", "Descripcion", false, false, "Link", "Link");
 
+// FUNCIONES LOCALSTORAGE
 const leerLocalStorage = (nombreLocalStorage) => {
     let arregloObjetos = JSON.parse(localStorage.getItem(nombreLocalStorage));
     if (arregloObjetos === null) {
@@ -35,23 +29,30 @@ const guardarLocalStorage = (nombreLocalStorage, arregloObjetos) => {
 // const eliminarLocalStorage = () => {}
 // const limpiarLocalStorage = () => {localStorage.clear();}
 
-// const botonAgregar = document.getElementById("botonAgregar");
-// botonAgregar.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     const nombre = document.getElementById("nombre");
-//     const categoria = document.getElementById("categoria");
-//     const descripcion = document.getElementById("descripcion");
-//     const publicado = document.getElementById("publicado");
-//     const destacado = document.getElementById("destacado");
-//     const imagen = document.getElementById("imagen");
-//     const url = document.getElementById("url");
+// FUNCIONES ADMINISTRACION PRODUCTOS
+const crearProducto = () => {
+    const nombre = document.getElementById("nombre").value;
+    const categoria = document.getElementById("categoria").value;
+    const descripcion = document.getElementById("descripcion").value;
+    const publicado = document.getElementById("publicado").checked;
+    const destacado = document.getElementById("destacado").checked;
+    const imagen = document.getElementById("imagen").value;
+    const url = document.getElementById("url").value;
 
-//     let producto = new Producto(123, nombre, categoria, descripcion, publicado, destacado, imagen, url); // Generar codigo aleatorio
-//     const productos = []; // Obtengo productos del localstorage 
-//     if (productos.find(element => element.nombre === producto.nombre))
-//         console.log("Este producto ya se encuentra"); // Mandar mensaje de alerta al modal
-//     else {
-//         productos.push(producto);
-//         guardarProductoLocalStorage(); // guardar en el localstorage el nuevo arreglo
-//     }
-// })
+    let producto = new Producto(1234, nombre, categoria, descripcion, publicado, destacado, imagen, url); // Generar codigo aleatorio por libreria o manual?
+    return producto;
+}
+
+// FUNCIONES BOTONES ADMINISTRACION PRODUCTOS 
+const botonAgregar = document.getElementById("botonAgregar");
+botonAgregar.addEventListener("click", (event) => {
+    event.preventDefault();
+    const producto = crearProducto();
+    const productos = leerLocalStorage("Pelicula");
+    if (productos.find(element => element.codigo === producto.codigo))
+        console.log("Este producto ya se encuentra"); // Mandar mensaje de alerta al modal
+    else {
+        productos.push(producto);
+        guardarLocalStorage("Pelicula", productos);
+    }
+})
