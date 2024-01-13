76c112d5434bdc1fff1ae7530056818b960f7e61
LucianoCV01 <Lucianocosentino01@gmail.com>
2024-01-13 17:16:02 -0300

    Correcciones en CRUD aplicadas

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ class Producto {
         this.categoria = categoria;
         this.descripcion = descripcion;
         this.publicado = publicado;
-        this.destacado = destacado;
+        this.destacado =  destacado || false;
         this.imagen = imagen;
         this.url = url;
     }
@@ -22,11 +22,18 @@ const leerLocalStorage = (nombreLocalStorage) => {
 const guardarLocalStorage = (nombreLocalStorage, arregloObjetos) => {
     localStorage.setItem(nombreLocalStorage, JSON.stringify(arregloObjetos));
 }
-
 // const eliminarLocalStorage = () => {}
 // const limpiarLocalStorage = () => {localStorage.clear();}
 
 // FUNCIONES EXTRA
+const generarCodigo = () => {
+    const productos = leerLocalStorage("Pelicula");
+    let ultimoCodigo = 0;
+    if (productos.length > 0) {
+        ultimoCodigo = productos[productos.length - 1].codigo;
+    }
+    return (ultimoCodigo + 1);
+}
 const buscarProductoPorCodigo = (codigoBuscado) => {
     const productos = leerLocalStorage("Pelicula");
     const productoEncontrado = productos.find(producto => producto.codigo === codigoBuscado);
@@ -36,14 +43,41 @@ const buscarProductoPorCodigo = (codigoBuscado) => {
     }
     return null;
 }
-const generarCodigo = () => {
+const construirFilaTabla = (producto, indice) => {
+    return `
+        <tr>
+            <td>${producto.codigo}</td>
+            <td>${producto.nombre}</td>
+            <td>${producto.categoria}</td>
+            <td>${producto.descripcion}</td>
+            <td><input type="checkbox" ${producto.publicado ? 'checked' : ''} onchange="publicarPelicula(${producto.codigo})"></td>
+            <td>
+                <div>
+                    <button type="button" class="btn btn-warning" onclick="destacarProducto(${producto.codigo})"><i class="${producto.destacado ? 'fa-solid fa-star' : 'fa-regular fa-star'}"></i></button>
+                </div>
+            </td>
+            <td>
+                <div>
+                    <button type="button" class="btn btn-success" data-bs-target="#miModal"
+                    onclick="mostrarModal('Modificar Producto', buscarProductoPorCodigo(${producto.codigo}))"><i class="fa-solid fa-pen-to-square"></i></button>
+                </div>
+            </td>
+            <td>
+                <div>
+                    <button type="button" class="btn btn-danger" onclick="eliminarPelicula(${indice})"><i class="fa-solid fa-trash"></i></button>
+                </div>
+            </td>
+        </tr>
+    `;
+};
+const mostrarPeliculasEnTabla = () => {
+    tbody.innerHTML = "";
     const productos = leerLocalStorage("Pelicula");
-    let ultimoCodigo = 0;
-    if (productos.length > 0) {
-        ultimoCodigo = productos[productos.length - 1].codigo;
+    for (let i = 0; i < productos.length; i++) {
+        const filaHTML = construirFilaTabla(productos[i], i);
+        tbody.innerHTML += filaHTML;
     }
-    return (ultimoCodigo + 1);
-}
+};
 // FUNCIONES ADMINISTRACION PRODUCTOS
 const crearProducto = () => {
     const nombre = document.getElementById("nombre").value;
@@ -60,65 +94,40 @@ const crearProducto = () => {
 const destacarProducto = (codigoProducto) => {
     let productos = leerLocalStorage("Pelicula");
     productos.forEach(producto => {
-        producto.destacado = false;
         if (codigoProducto === producto.codigo) {
             producto.destacado = true;
+        } else{
+            producto.destacado = false;
         }
     });
-    console.log(productos);
     guardarLocalStorage("Pelicula", productos);
+    console.log(productos);
+    mostrarPeliculasEnTabla();
 }
 const eliminarPelicula = (posicion) => {
     let arregloPeliculas = leerLocalStorage("Pelicula");
     arregloPeliculas.splice(posicion, 1)
-    console.log(arregloPeliculas)
     guardarLocalStorage("Pelicula", arregloPeliculas);
-    mostrarPeliculasEnTabla()
+    console.log(productos);
+    mostrarPeliculasEnTabla();
 }
-const publicarPelicula = (posicion, boton) => {
-    let arregloPeliculas = leerLocalStorage("Pelicula");
-    if (boton.checked) {
-        arregloPeliculas[posicion].publicado = 1
-    } else {
-        arregloPeliculas[posicion].publicado = 0
-    }
-    guardarLocalStorage("Pelicula", arregloPeliculas);
-    console.log(arregloPeliculas);
+const publicarPelicula = (codigoProducto) => {
+    let productos = leerLocalStorage("Pelicula");
+    productos.forEach(producto => {
+        if (codigoProducto === producto.codigo) {
+            if (producto.publicado) {
+                producto.publicado = false;
+            } else {
+                producto.publicado = true;
+            }
+        }
+    });
+    guardarLocalStorage("Pelicula", productos);
+    console.log(productos);
     mostrarPeliculasEnTabla();
 }
 
-const construirFilaTabla = (producto, indice) => {
-    return `
-        <tr>
-            <td>${producto.codigo}</td>
-            <td>${producto.nombre}</td>
-            <td>${producto.categoria}</td>
-            <td>${producto.descripcion}</td>
-            <td><input type="checkbox" ${producto.publicado ? 'checked' : ''} onchange="publicarPelicula(${indice}, this)"></td>
-            <td>
-                <div>
-                    <button class="btnEliminar" onclick="eliminarPelicula(${indice})"><i class="fa-solid fa-trash"></i></button>
-                    <button type="button" class="btn btn-success" data-bs-target="#miModal"
-                    onclick="mostrarModal('Modificar Producto', buscarProductoPorCodigo(${producto.codigo}))">Modificar</button>
-                    <button class="btnDestacar" onclick="destacarProducto(${producto.codigo})"><i class="${producto.destacado ? 'fa-solid fa-star' : 'fa-regular fa-star'}"></i></button>
-                </div>
-            </td>
-        </tr>
-    `;
-};
-const mostrarPeliculasEnTabla = () => {
-    tbody.innerHTML = "";
-    const productos = leerLocalStorage("Pelicula");
-    for (let i = 0; i < productos.length; i++) {
-        const filaHTML = construirFilaTabla(productos[i], i);
-        tbody.innerHTML += filaHTML;
-    }
-};
-
 mostrarPeliculasEnTabla();
-// FUNCIONES BOTONES ADMINISTRACION PRODUCTOS 
-
-
 // MODAL
 let modal
 const mostrarModal = (titulo, datosProducto) => {
@@ -137,6 +146,8 @@ const mostrarModal = (titulo, datosProducto) => {
         document.getElementById('imagen').value = datosProducto.imagen;
         document.getElementById('url').value = datosProducto.url;
         modalBtnAccion.textContent = "Actualizar";
+        document.getElementById('publicado').parentElement.style.display = 'none';
+        document.getElementById('destacado').parentElement.style.display = 'none';
     } else {
         // Limpiar el formulario si no hay datos existentes
         document.getElementById('formularioProductos').reset()
@@ -144,7 +155,6 @@ const mostrarModal = (titulo, datosProducto) => {
     }
     modal.show();
 }
-
 modalBtnAccion.addEventListener("click", (event) => {
     event.preventDefault();
     let productos = leerLocalStorage("Pelicula");
@@ -153,6 +163,9 @@ modalBtnAccion.addEventListener("click", (event) => {
         if (buscarProductoPorCodigo(producto.codigo) !== null)
             console.log("Este producto ya se encuentra");
         else {
+            if (producto.destacado) {
+                destacarProducto(producto.codigo);
+            }
             productos.push(producto);
         }
     } else {
@@ -160,15 +173,20 @@ modalBtnAccion.addEventListener("click", (event) => {
         const index = productos.findIndex(prod => Number(prod.codigo) === Number(codigoAModificar));
         if (index !== -1) {
             producto.codigo = codigoAModificar;
-            productos[index] = producto;
+            productos[index].nombre = producto.nombre;
+            productos[index].categoria = producto.categoria;
+            productos[index].descripcion = producto.descripcion;
+            productos[index].publicado = producto.publicado;
+            productos[index].destacado = producto.destacado;
+            productos[index].imagen = producto.imagen;
+            productos[index].url = producto.url;
         }
         else {
             console.log("Error al intentar modificar un producto");
         }
     }
     guardarLocalStorage("Pelicula", productos);
-    if (producto.destacado) {
-        destacarProducto(producto.codigo);
-    }
     modal.hide();
+    console.log(productos);
+    mostrarPeliculasEnTabla();
 })
