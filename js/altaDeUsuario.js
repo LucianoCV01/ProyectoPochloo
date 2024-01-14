7ff6b339d5f27951bcae09f53f1fdb94c1838652
Martina Gutierrez <martugutierrez166@gmail.com>
2024-01-14 18:55:06 -0300

    Agregando estilos

--- js/altaDeUsuario.js
+++ js/altaDeUsuario.js
@@ -7,6 +7,8 @@ import registro from "./exportAlta.js";
 const aceptarAlta = (i) =>
 {
     registro[i].estado = "Aceptado"
+    registro.push(registro[i])
+    registro.splice(i, 1)
     localStorage.setItem("Registro", JSON.stringify(registro))
     mostrarUsuariosEnTabla()
     console.log("sucede aceptarAlta")
@@ -15,6 +17,8 @@ const aceptarAlta = (i) =>
 const rechazarAlta = (i) =>
 {
     registro[i].estado = "Rechazado"
+    registro.push(registro[i])
+    registro.splice(i, 1)
     localStorage.setItem("Registro", JSON.stringify(registro))
     mostrarUsuariosEnTabla()
     console.log("Sucede rechazarAlta")
@@ -24,6 +28,15 @@ const crearFilasAlta = (usuario, indice) => {
     
     const fila = document.createElement("tr");
     
+    if(registro[indice].estado === "Aceptado")
+    {
+        fila.className = "table-success"
+    }else{
+        if (registro[indice].estado === "Rechazado") {
+            fila.className = "table-danger"
+        }
+    }
+
     const celdaUsuario = document.createElement("td");
     celdaUsuario.textContent = usuario.usuario;
     
@@ -32,18 +45,23 @@ const crearFilasAlta = (usuario, indice) => {
     
     const celdaBotones = document.createElement("td");
     
-    const botonAceptar = document.createElement("button");
-    botonAceptar.className = "col-2";
-    botonAceptar.addEventListener("click", () => aceptarAlta(indice));
-    botonAceptar.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+    if (registro[indice].estado === "Pendiente") {
+        const botonAceptar = document.createElement("button");
+        botonAceptar.className = "col-2 btn btn-success";
+        botonAceptar.addEventListener("click", () => aceptarAlta(indice));
+        botonAceptar.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
     
-    const botonRechazar = document.createElement("button");
-    botonRechazar.className = "col-2";
-    botonRechazar.addEventListener("click", () => rechazarAlta(indice));
-    botonRechazar.innerHTML = '<i class="fa-regular fa-circle-xmark"></i>';
+        const botonRechazar = document.createElement("button");
+        botonRechazar.className = "col-2 btn btn-danger";
+        botonRechazar.addEventListener("click", () => rechazarAlta(indice));
+        botonRechazar.innerHTML = '<i class="fa-regular fa-circle-xmark"></i>';
 
-    celdaBotones.appendChild(botonAceptar);
-    celdaBotones.appendChild(botonRechazar);
+        celdaBotones.appendChild(botonAceptar);
+        celdaBotones.appendChild(botonRechazar);
+    
+    }else{
+        celdaBotones.textContent = "-"
+    }
     
     fila.appendChild(celdaUsuario);
     fila.appendChild(celdaEstado);
