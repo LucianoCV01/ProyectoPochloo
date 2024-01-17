220953a7cd716e9fc1dbd728918cb9139b01eb56
Enzo Castro <enzocastro110@gmail.com>
2024-01-17 19:46:27 -0300

    a

--- Js/login.js
+++ Js/login.js
@@ -1,1 +1,17 @@
 const usuariosLogueados = JSON.parse(localStorage.getItem('Usuario'));
+
+function iniciarSesionPochloo() {
+    const email = document.getElementById('inputEmail').value;
+    const pass = document.getElementById('inputPassword').value;
+    fetch("../json/fakeApiEnzo")
+        .then(response => response.json())
+        .then(users => {
+            const userLogeadoPochloo = users.find(user => user.email === email && user.clave === pass);
+
+            if (userLogeadoPochloo) {
+                console.log("Usuario logueado:", userLogeadoPochloo);
+            } else {
+                console.log("Este usuario no está en el sistema");
+            }
+        });
+}
