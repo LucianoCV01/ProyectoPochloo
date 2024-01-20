39c3f15ec48f7f59890cb2662aced4d4a03617b8
Enzo Castro <enzocastro110@gmail.com>
2024-01-20 20:53:51 -0300

    avanzando

--- Js/login.js
+++ Js/login.js
@@ -5,15 +5,25 @@ function iniciarSesionPochloo() {
     const pass = document.getElementById('inputPassword').value;
     fetch("../json/fakeApiEnzo.json")
         .then(response => response.json())
-        .then(users => {
-            const userLogeadoPochloo = users.find(user => user.email === email && user.clave === pass);
+        .then(DATOS => {
+            const userLogeadoPochloo = DATOS.find(usuarioo => usuarioo.email === email && usuarioo.clave === pass);
             if (userLogeadoPochloo) {
-                console.log("Usuario logueado:", userLogeadoPochloo);
+                localStorage.setItem("USUARIO" , JSON.stringify(userLogeadoPochloo));
+                Swal.fire({
+                    title: "Bienvenido!!!",
+                    text: "Te haz logueado con éxito, bienvenido " +  userLogeadoPochloo.nombre,
+                    icon: "success",
+                })
+                console.log("El usuario se ha logueado:", userLogeadoPochloo);
             } else {
+                Swal.fire({
+                    title: "Usuario y/o contraseña incorrectos!",
+                    text: "Intenta nuevamente",
+                    icon: "error",
+                })
                 console.log("Este usuario no está en el sistema");
             }
         })
-        .catch(error => {
-            console.error("Error al obtener los datos de la API:", error);
-        });
+        .catch(error => console.log(error)
+        );
 }
