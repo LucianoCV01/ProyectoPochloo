26bf0050c43f4e674e23ffcd026a1ecd7f71aa7a
Martina Gutierrez <martugutierrez166@gmail.com>
2024-01-24 16:31:53 -0300

    Terminado de funciones + agregando películas al archivo json

--- js/categoria.js
+++ js/categoria.js
@@ -1,23 +1,38 @@
 const sectionPeliculas = document.getElementById("peliculasPorCategoria")
 
 let seleccion = localStorage.getItem("Categoria")
-
 let peliculas = JSON.parse(localStorage.getItem("Pelicula"))
 
+let article = document.createElement("article")
 
-let h2 = document.createElement("h2")
-h2.textContent = 'Mostrando películas de "' + seleccion + '"'
-sectionPeliculas.appendChild(h2)
+article.className = "d-flex flex-column flex-md-row flex-wrap"
+
+let divh1 = document.createElement("div")
+divh1.className = "justify-content-start p-4"
+let h1 = document.createElement("h1")
+h1.textContent = 'Mostrando películas de "' + seleccion + '"'
+divh1.appendChild(h1)
+sectionPeliculas.appendChild(divh1)
 
 
 for (let i = 0; i < peliculas.length; i++) {
     if(seleccion.toLowerCase() === peliculas[i].categoria.toLowerCase())
     {
         let div = document.createElement("div")
+        let nombre = document.createElement("h3")
+        let a = document.createElement("a")
         let img = document.createElement("img")
+        nombre.textContent = peliculas[i].nombre
+        nombre.className = "ps-2 pb-5 pt-2"
+        div.className = "mx-auto col-md-6"
+        a.href = "../html/detalle.html"
         img.src = peliculas[i].imagen
         img.alt = "Portada de la película " + peliculas[i].nombre
-        div.appendChild(img)
-        sectionPeliculas.appendChild(div)
+        a.appendChild(img)
+        div.appendChild(a)
+        div.appendChild(nombre)
+        article.appendChild(div)
     }
 }
+
+sectionPeliculas.appendChild(article)
