f53346d464318a86162fd532b81e5954104ce500
Enzo Castro <enzocastro110@gmail.com>
2024-01-30 20:02:39 -0300

    notas

--- Js/login.js
+++ Js/login.js
@@ -1,14 +1,18 @@
 const usuariosLogueados = JSON.parse(localStorage.getItem('Usuario'));
 
+//obtengo los datos con "getelementById"//
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
     iniciarSesionPochloo();
 });
 
+
+//funcion para iniciar sesión//
 function iniciarSesionPochloo() {
     const email = document.getElementById('inputEmail').value;
     const pass = document.getElementById('inputPassword').value;
 
+    //verifico los espacios para que no haya nada vacio//
     if (email.trim() === '' || pass.trim() === '') {
         Swal.fire({
             title: "Error al iniciar sesión",
@@ -17,6 +21,7 @@ function iniciarSesionPochloo() {
         });
         return;}
 
+        //verifico que haya un arroba en el mail//
     if (!email.includes('@')) {
             Swal.fire({
                 title: "Formato de correo incorrecto",
@@ -27,11 +32,11 @@ function iniciarSesionPochloo() {
         }
 
     fetch("../json/fakeApiEnzo.json")
-        .then(response => response.json())
+        .then(response => response.json()) //aquí convierto la respuesta del fetch en formado de Javascript y luego con el DATOS manejo los datos//
         .then(DATOS => {
             const userLogeadoPochloo = DATOS.find(usuarioo => usuarioo.email === email && usuarioo.clave === pass);
             if (userLogeadoPochloo) {
-                localStorage.setItem("Usuario" , JSON.stringify(userLogeadoPochloo));
+                localStorage.setItem("Usuario" , JSON.stringify(userLogeadoPochloo)); //al usar el JSON.stringify lo convertimos en formato JSON para el local storage//
                 Swal.fire({
                     title: "Bienvenido!!!",
                     text: "Te has logueado con éxito, bienvenido " +  userLogeadoPochloo.nombre,
